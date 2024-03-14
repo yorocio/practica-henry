604ea2033ff883b940a7bb75e366363e3891cc5c
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:49:36 -0300

    concatenar y saludar

--- C5 Fundamentos Javascript/ejercicios/28.js
+++ C5 Fundamentos Javascript/ejercicios/28.js
@@ -2,6 +2,8 @@ function obtenerSaludo(nombre) {
   // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
   // Ejemplo: "Martin" ---> "Hola Martin!"
   // Tu código:
+  let saludar = 'Hola ' + nombre + '!';
+  return saludar;
 }
 
 module.exports = obtenerSaludo;
