b9c38cb8f37d9dade306cfe28ffed120bf0df2c8
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:41:13 -0300

    resta

--- C5 Fundamentos Javascript/ejercicios/22.js
+++ C5 Fundamentos Javascript/ejercicios/22.js
@@ -2,6 +2,8 @@ function resta(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de la resta.
   // Tu código:
+  let resta1 = x - y;
+  return resta1;
 }
 
 module.exports = resta;
