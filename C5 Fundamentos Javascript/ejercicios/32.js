4ba60828631e1cc8c703c6e42da717e24d64d02a
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:57:38 -0300

    pasar de euro a dolar y calcular el valor

--- C5 Fundamentos Javascript/ejercicios/32.js
+++ C5 Fundamentos Javascript/ejercicios/32.js
@@ -2,6 +2,8 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Supongamos que 1 euro equivale a 1.20 dólares.
   // Tu código:
+  let calcularEuroAdolar = euro * 1.20;
+return calcularEuroAdolar;
 }
 
 module.exports = deEuroAdolar;
