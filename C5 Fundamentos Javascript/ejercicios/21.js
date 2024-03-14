ffa31115d39bdf13e0879934f45e320012cab12c
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:40:20 -0300

    suma

--- C5 Fundamentos Javascript/ejercicios/21.js
+++ C5 Fundamentos Javascript/ejercicios/21.js
@@ -2,6 +2,8 @@ function suma(x, y) {
   // La función recibe dos argumentos "x" e "y" que son números.
   // Retorna el resultado de su suma.
   // Tu código:
+  let suma1 = x + y;
+  return suma1;
 }
 
 module.exports = suma;
