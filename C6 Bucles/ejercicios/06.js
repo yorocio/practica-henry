adf5a3ad878b7a2bc6360d154214b610ca50c3b8
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 13:03:25 -0300

    usando el  metodo toString().

--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,13 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  let numeroAstring = num.toString();
+  if (numeroAstring.length == 3){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = tieneTresDigitos;
