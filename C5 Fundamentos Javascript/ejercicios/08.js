754071a8aaeed79c74c73c2557f20608f484ce36
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 10:44:15 -0300

    numero entero

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,11 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  if (typeof numero === 'number' && numero % 1 == 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = esNumeroEntero;
