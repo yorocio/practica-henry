9d8be79d120fae557a3edefc7392ee8906963fcb
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 10:59:31 -0300

    metodo Date.parse

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,15 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  if(typeof fecha == 'string'){
+    return false;
+  }
+  if (Date.parse(fecha)){
+    return true;
+}
+else {
+  return false;
+}
 }
 
 module.exports = esFechaValida;
