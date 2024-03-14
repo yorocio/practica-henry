41e59b07d3668f6b663888a09fc891bbefb595e8
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 12:48:16 -0300

    obtener el numero mayor

--- C5 Fundamentos Javascript/ejercicios/33.js
+++ C5 Fundamentos Javascript/ejercicios/33.js
@@ -6,6 +6,7 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  
 }
 
 module.exports = esNumeroPrimo;
--- C6 Bucles/ejercicios/03.js
+++ C6 Bucles/ejercicios/03.js
@@ -3,6 +3,13 @@ function obtenerMayor(x, y) {
   // Retorna el número más grande.
   // Si son iguales, retornar cualquiera de los dos.
   // Tu código:
+  if (x > y){
+    return x;
+  } else if (y > x){
+    return y;
+  } else if (x === y) {
+    return x;
+  }
 }
 
 module.exports = obtenerMayor;
