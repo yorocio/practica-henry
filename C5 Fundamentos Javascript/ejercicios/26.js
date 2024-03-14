8819589d32b9909a9eaebb78297c82fc74aeae27
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:45:43 -0300

    agregar signo de exclamacion

--- C5 Fundamentos Javascript/ejercicios/26.js
+++ C5 Fundamentos Javascript/ejercicios/26.js
@@ -2,6 +2,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final del string "str" y retórnalo.
   // Ejemplo: "hello world" ---> "hello world!"
   // Tu código:
+  let concat = str + '!';
+  return concat;
 }
 
 module.exports = agregarSimboloExclamacion;
