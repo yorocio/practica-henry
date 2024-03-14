d9ee0fec3d13356eee109a84ab9e26e7c0cfc734
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:51:31 -0300

    calcular el area del rectangulo

--- C5 Fundamentos Javascript/ejercicios/29.js
+++ C5 Fundamentos Javascript/ejercicios/29.js
@@ -1,6 +1,8 @@
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el área de un rectángulo teniendo su alto y ancho.
   // Tu código:
+  let areadelRectangulo = alto * ancho;
+  return areadelRectangulo;
 }
 
 module.exports = obtenerAreaRectangulo;
