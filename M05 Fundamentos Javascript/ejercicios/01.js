d1bbedbd5ef33672c4c6dcee3168133d78df4890
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 10:09:47 -0300

    cambia la nueva estructura henry

--- M05 Fundamentos Javascript/ejercicios/01.js
+++
@@ -1,6 +0,0 @@
-// Crea una variable de tipo string.
-// Reemplaza el valor de null por el correspondiente.
-
-const nuevoString = null;
-
-module.exports = nuevoString;
--- M05 Fundamentos Javascript/ejercicios/30.js
+++
@@ -1,7 +0,0 @@
-function retornarPerimetro(lado) {
-  // La función recibe como argumento la medida de un lado de un cuadrado.
-  // Debes retornar su perímetro.
-  // Tu código:
-}
-
-module.exports = retornarPerimetro;
