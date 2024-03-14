91d243bfed2b94ccf98929548ac5d932fe27ca07
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 10:45:53 -0300

    uso d eif else

--- C5 Fundamentos Javascript/ejercicios/09.js
+++ C5 Fundamentos Javascript/ejercicios/09.js
@@ -2,7 +2,13 @@ function esNuloOIndefinido(valor) {
   // La función recibe un argumento "valor".
   // Si este valor es null o undefined, retorna true.
   // De lo contrario, retorna false.
-  // Tu código:
+  // Tu código:§
+  if (valor == null || valor == undefined){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = esNuloOIndefinido;
