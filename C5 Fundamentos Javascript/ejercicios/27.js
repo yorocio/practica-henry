73081b1c3ed6325605e0316a791b7a76c5037818
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:47:14 -0300

    combinar nombres con espacio

--- C5 Fundamentos Javascript/ejercicios/27.js
+++ C5 Fundamentos Javascript/ejercicios/27.js
@@ -2,6 +2,8 @@ function combinarNombres(nombre, apellido) {
   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
   // Tu código:
+  let concatenarString = nombre + ' ' + apellido;
+  return concatenarString;
 }
 
 module.exports = combinarNombres;
