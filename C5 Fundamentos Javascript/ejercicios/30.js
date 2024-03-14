9e0b1de240e76a35068404f09915d77e08a88d44
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:52:45 -0300

    calcular el perimetro del cuadrado

--- C5 Fundamentos Javascript/ejercicios/30.js
+++ C5 Fundamentos Javascript/ejercicios/30.js
@@ -2,6 +2,8 @@ function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   let perimetroDelCuadrado = lado * 4;
+   return perimetroDelCuadrado;
 }
 
 module.exports = retornarPerimetro;
