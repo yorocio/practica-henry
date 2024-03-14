6ea66191add02c7ac13824d2ae283bf3d44130e6
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 17:42:51 -0300

    uso if, else if, else

--- C6 Bucles/ejercicios/17.js
+++ C6 Bucles/ejercicios/17.js
@@ -7,6 +7,21 @@ function colors(color) {
   // Si no es ninguno de esos colores           --> "Color not found".
   // PISTA: utilizar el statement SWITCH.
   // Tu código:
+  if (color == 'blue'){
+    return "This is blue";
+  } 
+  else if (color == 'red'){
+    return "This is red";
+  }
+  else if (color == 'green'){
+    return "This is green";
+  }
+  else if (color == 'orange'){
+    return "This is orange";
+  }
+  else {
+    return "Color not found";
+  }
 }
 
 module.exports = colors;
