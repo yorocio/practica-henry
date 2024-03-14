70c553892e49f4e3b29db800bcffed0ce9ad3383
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-14 11:55:54 -0300

    calcular el area del triangulo

--- C5 Fundamentos Javascript/ejercicios/31.js
+++ C5 Fundamentos Javascript/ejercicios/31.js
@@ -1,6 +1,8 @@
 function areaDelTriangulo(base, altura) {
   // Calcula el área de un triángulo y retorna el resultado.
   // Tu código:
+  let areaTriangulo = (base * altura) / 2;
+  return areaTriangulo;
 }
 
 module.exports = areaDelTriangulo;
