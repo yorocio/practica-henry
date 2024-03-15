311b548e1f5c9618271509f1505b824a99e577a2
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-15 11:32:38 -0300

    practicando do while

--- C6 Bucles/ejercicios/20.js
+++ C6 Bucles/ejercicios/20.js
@@ -3,6 +3,14 @@ function sumarHastaNConBreak(n) {
    // Devuelve la suma de todos los números desde 1 hasta n.
    // Si la suma supera a 100, detén el bucle usando break.
    // Tu código:
+   let sumaTotal = 0;
+  for (let i = 1; i <= n; i++) {
+    sumaTotal = sumaTotal + i;
+    if (sumaTotal > 100){
+      break;
+    }
+  }
+  return sumaTotal;
 }
 
 module.exports = sumarHastaNConBreak;
--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -3,6 +3,13 @@ function doWhile(num) {
   // Retorna el valor final.
   // PISTA: Utiliza el bucle do-while.
   // Tu código:
+  let i = 0;
+  do {
+    num = num + 5;
+    i++;
+  }
+  while (i < 8);
+  return num;
 }
 
 module.exports = doWhile;
