939a1c18dc705216ab2cb83956c7f93eef9a4dcb
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-15 12:16:35 -0300

    modulo 7 - arrays

--- C6 Bucles/ejercicios/25.js
+++ C6 Bucles/ejercicios/25.js
@@ -5,6 +5,15 @@ function esPalindromo(string) {
    // IMPORTANTE: Un palíndromo es una palabra o frase
    // que se lee igual hacia adelante que hacia atrás.
    // Tu código:
+let d = string.length - 1;
+let n = string.length
+for (let i = 0; i <= (n/2)-1; i++){
+   d = (n-1) - i;
+   if (string[i] !== string[d]){
+      return false;
+   } 
+}
+      return true;
 }
 
 module.exports = esPalindromo;
