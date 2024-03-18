1157a9682a52e17194098d3c83d7118487fc50a9
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-18 12:01:22 -0300

    modulo 8 - invocar callback

--- C7 Arrays/ejercicios/10.js
+++ C7 Arrays/ejercicios/10.js
@@ -1,6 +1,13 @@
 function obtenerPrimerStringLargo(array) {
   // Devuelve el primer string con más de 5 caracteres en el array.
   // Tu código:
-}
+  for(let i = 0; array.length; i++){
+    if(typeof array[i] === 'string' && array[i].length >= 5){
+      return array[i];
+    }
+  }
+  return undefined;
+  }
+
 
 module.exports = obtenerPrimerStringLargo;
