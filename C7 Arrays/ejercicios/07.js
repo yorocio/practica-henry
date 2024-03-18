18de2bde300dad497508702bebe4500bededb61c
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-18 08:38:39 -0300

    ordenar array - M7 ejercicio 7

--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,9 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  let arrayOrdenado = array.sort();
+  return arrayOrdenado;
+  
 }
 
 module.exports = ordenarArray;
