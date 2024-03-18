98ae5dfcba111b27e39a55fcb775e57481681c73
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-18 09:50:11 -0300

    numero aleatorio - M7 ejercicio 9

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,6 +2,13 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
+   let indiceAleatorio = Math.floor(Math.random(array) * array.length)
+   if(array.length <= 0){
+      return undefined;
+   }
+   else{
+   return array[indiceAleatorio] ;
+   }
 }
 
 module.exports = obtenerElementoAleatorio;
