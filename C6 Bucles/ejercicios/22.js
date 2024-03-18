f2c85d6a2b049be389e5fd81affbf32d42d64c89
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-03-18 08:30:07 -0300

    combine - Modulo 6 ejercicio 22

--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -6,6 +6,26 @@ function combine(str1, str2, str3) {
    // combine("abc", "", "123") == "a1b2c3"
    // combine("abc", "12345", "") == "a1b2c345"
    // combine("abc", "12345", "67") == "a16b27c345"
-}
+   let cadenas = [str1, str2, str3]
+   let masLargo = cadenas[0].length
 
+   for (let i = 0; i < cadenas.length; i++) {
+      if (cadenas[i].length > masLargo) {
+         masLargo = cadenas[i].length;
+      }
+   }
+   const arrayFinal = []
+   for (let i = 0; i < masLargo; i++) {
+      if (str1[i]) {
+         arrayFinal.push(str1[i]);
+      }
+      if (str2[i]) {
+         arrayFinal.push(str2[i]);
+      }
+      if (str3[i]) {
+         arrayFinal.push(str3[i]);
+      }
+   }
+   return arrayFinal.join('');
+}
 module.exports = combine;
