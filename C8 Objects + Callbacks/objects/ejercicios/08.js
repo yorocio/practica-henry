5d5c6c35289d22b1372bdd6c16eb9c683716e0f3
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-04-04 10:45:24 -0300

    creando un objeto

--- C8 Objects + Callbacks/objects/ejercicios/08.js
+++ C8 Objects + Callbacks/objects/ejercicios/08.js
@@ -5,6 +5,14 @@ function crearGato(nombre, edad) {
   // La propiedad "meow" será una función que retorne el string: "Meow!".
   // Retornar el objeto.
   // Tu código:
+  let nuevoObjeto = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      return 'Meow!';
+    }
+  };
+  return nuevoObjeto;
 }
 
 module.exports = crearGato;
