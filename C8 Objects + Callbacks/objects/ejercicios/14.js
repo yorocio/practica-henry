d09c0b9e7df71ea36377589631a2787c0252558b
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-04-05 09:33:42 -0300

    verificar si el objeto tiene propied email

--- C8 Objects + Callbacks/objects/ejercicios/14.js
+++ C8 Objects + Callbacks/objects/ejercicios/14.js
@@ -2,6 +2,6 @@ function tieneEmail(objetoUsuario) {
   // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
+  return objetoUsuario.email != undefined; 
 }
-
 module.exports = tieneEmail;
