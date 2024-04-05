f76d8d96d005c04bf7dfda35a7c79a5b2f0b0963
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2024-04-05 13:14:17 -0300

    objeto, propiedad con arreglo y agregar un elemento al final

--- C8 Objects + Callbacks/objects/ejercicios/16.js
+++ C8 Objects + Callbacks/objects/ejercicios/16.js
@@ -2,6 +2,11 @@ function verificarPassword(objetoUsuario, password) {
   // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
   // En ese caso, retorna true. Caso contrario, false.
   // Tu código:
+  if (objetoUsuario.password == password){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = verificarPassword;
